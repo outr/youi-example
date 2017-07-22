0f73dc9c3528b0346fc8bf4d19738ebb549265d9
Matt Hicks <matt@outr.com>
2017-07-22 13:29:05 -0500

    Removed unnecessary `cached` method from ClientExampleApplication.

--- js/src/main/scala/io/youi/example/ClientExampleApplication.scala
+++ js/src/main/scala/io/youi/example/ClientExampleApplication.scala
@@ -1,15 +1,11 @@
 package io.youi.example
 
-import io.youi.Cache
 import io.youi.app.ClientApplication
-import io.youi.net.URL
 
 import scala.scalajs.js.annotation.JSExportTopLevel
 
 object ClientExampleApplication extends ExampleApplication with ClientApplication {
   val hello = HelloScreen
-
-  override def cached(url: URL): String = Cache.cached(url)
 
   @JSExportTopLevel("application")
   def main(): Unit = {
